a628f317224b2137c7f1a6b3e4c90641f9087057
Nguyen Hoang Tuan <tuanmaiyeunguoido@gmail.com>
2024-07-22 09:55:26 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hook1 from './hook/Hook1'
+
+export default function App() {
+  return (
+    <div>
+    <Hook1/>
+    </div>
+  )
+}
---
+++ src/hook/Hook1.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+export default function Hook1() {
+    let [name, setName] =useState('');
+    let [a, setA] = useState('');
+    let [b, setB] = useState('');
+    let [result, setResult] = useState('');
+    let [textA, setTextA] = useState('');   
+    let [textB, setTextB] = useState('');
+  return (
+    <div>
+        <p>{name} {a} {b}</p>
+        <input type="text" placeholder='nhap ten di' onChange={(e)=>setName(e.target.value)}/><br/>
+
+        <input type="text" placeholder='' value={result} readOnly/><br/>
+        <input type="text" placeholder='nhap a' value={a}  onChange={(e)=>setA(e.target.value) } /><br/>
+        <input type="text" placeholder='nhap a' onChange={(e)=>setB(e.target.value)} /><br/>
+        <button onClick={()=>setResult((parseFloat(a)+parseFloat(b)).toFixed(2))}>+</button>
+        <button onClick={()=>setResult((parseFloat(a)-parseFloat(b)).toFixed(2))}>-</button>
+        <button onClick={()=>setResult((parseFloat(a)*parseFloat(b)).toFixed(2))}>*</button>
+        {/* <button onClick={()=>setResult((parseFloat(a)/parseFloat(b)).toFixed(2))}>/</button> */}
+        <button onClick={()=>setResult((parseFloat(a)/parseFloat(b)).toFixed(2))}>/</button>
+        <button onClick={()=>{
+        setResult("");
+         setA=('');
+        //  setB=("");
+        //     setTextA=("");
+        //     setTextB=("");
+         }}>clear</button>
+
+        
+    </div>
+  )
+}
